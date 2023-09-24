27baf459eae221beba7b8463e4bf113f0a01212f
Oncopoda <77498306+Oncopoda@users.noreply.github.com>
2023-09-24 18:15:21 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/test', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
