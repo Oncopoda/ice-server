7d23c45a9e188ae58db9dc42166e5b187f146158
Oncopoda <77498306+Oncopoda@users.noreply.github.com>
2023-09-24 18:30:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/test', (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
